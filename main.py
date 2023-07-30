bcd4a451ca65b4dac4c71cf96582d8a815dc5520
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2023-07-29 23:30:13 -0300

    add first surface

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("One of my games lmao")
 clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((100, 200))
+test_surface.fill((255, 0, 0))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    # draw all our elements
+    screen.blit(test_surface, (200, 100))
 
-    # updates the screen
     pygame.display.update()
     clock.tick(60)
